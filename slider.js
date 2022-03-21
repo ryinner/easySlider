3841a7cc70c2b6228ac54981ebe363c775ab17b5
Artem <vinograd18062002@gmail.com>
2022-03-21 14:19:38 +0500

    better

--- slider.js
+++ slider.js
@@ -11,18 +11,18 @@ function slider() {
     // Events to button
     dots.forEach((item) => {
         item.addEventListener('click', () => {
-            const id = item.getAttribute('data-id')-1
+            const id = item.getAttribute('data-id')
             checkSlide(slides, id)
         })
     })
     // Left button
     left.addEventListener('click', () => {
-        const id = document.querySelector('.active').getAttribute('data-id')-2
+        const id = document.querySelector('.active').getAttribute('data-id')-1
         checkSlide(slides ,id)
     })
     // Right button
     right.addEventListener('click', () => {
-        const id = document.querySelector('.active').getAttribute('data-id')
+        const id = +document.querySelector('.active').getAttribute('data-id')+1
         checkSlide(slides ,id)
     })
 }
@@ -33,15 +33,15 @@ function checkSlide(slides, id) {
         slide.classList.remove('active')
     })
     switch (id) {
-        case -1:
-            id = slides.length-1
+        case 0:
+            id = slides.length
             break;
     
         default:
-            if (id > slides.length-1) {
-                id = 0
+            if (id > slides.length) {
+                id = 1
             }
             break;
     }
-    slides[id].classList.add('active')
+    document.querySelector('.slide-container[data-id="'+ id +'"]').classList.add('active')
 }
