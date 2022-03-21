84956ff61d541ff7b047e90b70fe5ccae718d3e6
Artem <vinograd18062002@gmail.com>
2022-03-21 12:15:36 +0500

    Work

---
+++ slider.js
@@ -0,0 +1,47 @@
+// Init
+slider()
+
+// Init function
+function slider() {
+    // Variables
+    const slides = document.querySelectorAll('.slide-container')
+    const dots = document.querySelectorAll('.slider-dots__btn')
+    const left = document.querySelector('.slider__controll-left')
+    const right = document.querySelector('.slider__controll-right')
+    // Events to button
+    dots.forEach((item) => {
+        item.addEventListener('click', () => {
+            const id = item.getAttribute('data-id')-1
+            checkSlide(slides, id)
+        })
+    })
+    // Left button
+    left.addEventListener('click', () => {
+        const id = document.querySelector('.active').getAttribute('data-id')-2
+        checkSlide(slides ,id)
+    })
+    // Right button
+    right.addEventListener('click', () => {
+        const id = document.querySelector('.active').getAttribute('data-id')
+        checkSlide(slides ,id)
+    })
+}
+
+// Checker number of slide
+function checkSlide(slides, id) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+    switch (id) {
+        case -1:
+            id = slides.length-1
+            break;
+    
+        default:
+            if (id > slides.length-1) {
+                id = 0
+            }
+            break;
+    }
+    slides[id].classList.add('active')
+}
